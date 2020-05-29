b95540af6cf9ecf4676d984d39c3075ebb7deb43
Effie2019 <51808419+Effie2019@users.noreply.github.com>
2020-05-29 11:49:10 -0400

    Update Intro to sql.sql

--- Intro to sql.sql
+++ Intro to sql.sql
@@ -1,5 +1,23 @@
+#UNIQUE
 SELECT DISTINCT role
 FROM roles
 #Learning to COUNT
 SELECT COUNT(*)
 FROM people;
+#BETWEEN...AND... VS AND AND 
+WHERE release_year BETWEEN 1990 AND 2000
+#WHERE...IN VS OR OR 
+WHERE language IN ('English', 'Spanish', 'French')
+#NULL & IS NULL
+WHERE language IS NULL
+#LIKE & NOT LIKE
+WHERE name NOT LIKE 'A%';
+
+#SAMPLE
+SELECT release_year,AVG(budget) AS avg_budget,AVG(gross) AS avg_gross
+FROM films
+WHERE release_year>1990
+GROUP BY release_year
+HAVING AVG(budget)>	60000000
+ORDER BY AVG(gross)
+LIMIT 5
