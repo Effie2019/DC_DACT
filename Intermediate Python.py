2f1fe36e958f30721117996839be766b70b429a1
Effie2019 <51808419+Effie2019@users.noreply.github.com>
2020-05-30 22:38:51 -0400

    Update Intermediate Python.py

--- Intermediate Python.py
+++ Intermediate Python.py
@@ -20,3 +20,43 @@ plt.grid(True)
 plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) * 2,c = col,alpha=0.8)
 
 #2.Dictionaries & Pandas
+my_dict = {
+   "key1":"value1",
+   "key2":"value2",
+}
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
+# Add italy to europe
+europe['italy']='rome'
+del(europe['australia'])
+
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+# Create sub-dictionary data
+data={'capital':'rome','population':59.83}
+
+# Add data to europe under key 'italy'
+europe['italy']=data
+#3.logic, Control Flow and Filtering
+#(1)Boolean Operators with Numpy
+np.logical_and(my_house > 13, 
+               your_house < 15)
+#(2)if, elif, else
+if room == "kit" :
+    print("looking around in the kitchen.")
+elif room == "bed":
+    print("looking around in the bedroom.")
+else :
+    print("looking around elsewhere.")
+#(3)Filtering pandas DataFrames
+cpc=cars['cars_per_cap']
+between=np.logical_and(cpc > 100,cpc< 500)
+medium=cars[between]
