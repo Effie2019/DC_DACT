03e4eae58e48bc6fe8f559ea7edc6b1e92d3505b
Effie2019 <51808419+Effie2019@users.noreply.github.com>
2020-05-31 17:03:25 -0400

    Update Data Manipulation with pandas.py

--- Data Manipulation with pandas.py
+++ Data Manipulation with pandas.py
@@ -46,3 +46,11 @@ print(temperatures_ind.reset_index())
 
 # Reset the index, dropping its contents
 print(temperatures_ind.reset_index(drop=True))
+temperatures_ind.loc[['Moscow',"Saint Petersburg"]]
+
+# Index temperatures by country & city
+temperatures_ind = temperatures.set_index(["country","city"])
+# List of tuples: Brazil, Rio De Janeiro & Pakistan, Lahore
+rows_to_keep = [("Brazil", "Rio De Janeiro"), ("Pakistan", "Lahore")]
+# Subset for rows to keep
+print(temperatures_ind.loc[rows_to_keep])
