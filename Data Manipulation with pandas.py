471c9ef0eca9c2953ebfe210422da05b02cc7055
Effie2019 <51808419+Effie2019@users.noreply.github.com>
2020-06-02 11:14:06 -0400

    Update Data Manipulation with pandas.py

--- Data Manipulation with pandas.py
+++ Data Manipulation with pandas.py
@@ -4,129 +4,61 @@
 .columns: An index of columns: the column names.
 .index: An index for the rows: either row numbers or row names.
 #(2)Sorting rows
-homelessness_reg_fam = homelessness.sort_values(['region','family_members'],ascending=[True,False])  
-#(3)Subsetting rows
+homelessness_reg_fam = homelessness.sort_values(['region','family_members'],ascending=[True,False]) 
+#(3)Subsetting columns
+ind_state = homelessness[["individuals","state"]]
+#(4)Subsetting rows
 fam_lt_1k_pac = homelessness[(homelessness['family_members']<1000) &
                              ( homelessness['region']=='Pacific')]
-mojave_homelessness = homelessness[homelessness['state'].isin(canu)]
+cities =['Moscow',"Saint Petersburg"]
+print(temperatures[temperatures["city"].isin(cities)])
+
 #2.Summary Statistics
-import numpy as np
+#(1)agg.
 def iqr(column):
-    return column.quantile(0.75) - column.quantile(0.25)
-# Update to print IQR and median of temperature_c, fuel_price_usd_per_l, & unemployment
+  return column.quantile(0.75) - column.quantile(0.25)
 print(sales[["temperature_c", "fuel_price_usd_per_l", "unemployment"]].agg([iqr,np.median]))
-Cumulative statistics
-sales_1_1["cum_weekly_sales"] = sales_1_1["weekly_sales"].cumsum()
-
-# Get the cumulative max of weekly_sales, add as cum_max_sales col
-sales_1_1["cum_max_sales"] = sales_1_1["weekly_sales"].cummax()
-
-# Count the number of each department number and sort
-dept_counts_sorted = departments["department_num"].value_counts(sort=True)
-print(dept_counts_sorted)
-
-# Get the proportion of departments of each number and sort
-dept_props_sorted = departments["department_num"].value_counts(sort=True, normalize=True)
-print(dept_props_sorted)
-
-# Import NumPy with the alias np
-import numpy as np
-unemp_fuel_stats = sales.groupby('type')["unemployment","fuel_price_usd_per_l"].agg([np.min, np.max, np.mean,np.median])
-# Print unemp_fuel_stats
-print(unemp_fuel_stats)
-
-#3
-temperatures_ind = temperatures.set_index("city")
-
-# Look at temperatures_ind
-print(temperatures_ind)
-
-# Reset the index, keeping its contents
-print(temperatures_ind.reset_index())
-
-# Reset the index, dropping its contents
-print(temperatures_ind.reset_index(drop=True))
+sales_stats = sales.groupby('type')["weekly_sales"].agg([np.min, np.max, np.mean,np.median])
+#(2)Cumulative statistics
+sales_1_1["weekly_sales"].cumsum()
+sales_1_1["weekly_sales"].cummax()
+#(3)Dropping duplicates
+sales.drop_duplicates(subset=["store","department"])
+#(3)Count the number of each department number and sort
+departments["department_num"].value_counts(sort=True, normalize=True)
+#(4)Calculations with .groupby()
+sales.groupby("type")["weekly_sales"].sum()
+#(5)Pivot Table
+sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0, margins=True)
+temp_by_country_city_vs_year = temperatures.pivot_table("avg_temp_c",index=["country","city"],columns="year")
+#3 Slicing and indexing
+#(1)index
+temperatures.set_index("city")
+temperatures.set_index(["country","city"])
+temperatures_ind.reset_index()# Reset the index, keeping its contents
+temperatures_ind.reset_index(drop=True)# Reset the index, dropping its contents
 temperatures_ind.loc[['Moscow',"Saint Petersburg"]]
-
-# Index temperatures by country & city
+#(2)Setting multi-level indexs
 temperatures_ind = temperatures.set_index(["country","city"])
-# List of tuples: Brazil, Rio De Janeiro & Pakistan, Lahore
-rows_to_keep = [("Brazil", "Rio De Janeiro"), ("Pakistan", "Lahore")]
-# Subset for rows to keep
+rows_to_keep = [("Brazil", "Rio De Janeiro"), ("Pakistan", "Lahore")]# List of tuples
 print(temperatures_ind.loc[rows_to_keep])
-
-# Sort the index of temperatures_ind
+#(3)Sort the index 
 temperatures_srt = temperatures_ind.sort_index()
+#(4)Slicing rows
 # Subset rows from Pakistan to Russia
 print(temperatures_srt.loc["Pakistan":"Russia"])
-# Try to subset rows from Lahore to Moscow
-print(temperatures_srt.loc["Lahore":"Moscow"])
 # Subset rows from Pakistan, Lahore to Russia, Moscow
 print(temperatures_srt.loc[("Pakistan", "Lahore"):("Russia", "Moscow")])
-# Subset rows from India, Hyderabad to Iraq, Baghdad
-print(temperatures_srt.loc[("India", "Hyderabad"):("Iraq","Baghdad")])
-
+#(5)Slicing columns
 # Subset columns from date to avg_temp_c
 print(temperatures_srt.loc[:,"date":"date to avg_temp_c"])
-
 # Subset in both directions at once
 print(temperatures_srt.loc[("India", "Hyderabad"):("Iraq","Baghdad"),"date":"date to avg_temp_c"])
+# (6).iloc
+# Use slicing in both directions at once
+print(temperatures.iloc[0:5,2:4])
 
-#4.
-# Modify bins to 20
-avocados[avocados["type"] == "conventional"]["avg_price"].hist(alpha=0.5, bins=20)
-
-# Modify bins to 20
-avocados[avocados["type"] == "organic"]["avg_price"].hist(alpha=0.5,bins=20)
-
-# Add a legend
-plt.legend(["conventional", "organic"])
-
-# Show the plot
-plt.show()
-# Import matplotlib.pyplot with alias plt
-import matplotlib.pyplot as plt
-
-# Check individual values for missing values
-print(avocados_2016.isna())
-
-# Check each column for missing values
-print(avocados_2016.isna().any())
-
-# Bar plot of missing values by variable
-avocados_2016.isna().sum().plot(kind="bar")
-
-# Show plot
-plt.show()
-# Fill in missing values with 0
-avocados_filled = avocados_2016.fillna(0)
-
-# Create histograms of the filled columns
-avocados_filled[cols_with_missing].hist()
-plt.show()
-
-# Show the plot
-plt.show()
-# Create a list of dictionaries with new data
-avocados_list = [
-    {"date": "2019-11-03", "small_sold": 10376832, "large_sold": 7835071},
-    {"date": "2019-11-10", "small_sold": 10717154, "large_sold": 8561348},
-]
-
-# Convert list into DataFrame
-avocados_2019 = pd.DataFrame(avocados_list)
-
-# Print the new DataFrame
-print(avocados_2019)
-# Create a dictionary of lists with new data
-avocados_dict = {
-  "date": ["2019-11-17","2019-12-01"],
-  "small_sold": [10859987,9291631],
-  "large_sold": [7674135,6238096]
-}
-
-# Convert dictionary into DataFrame
-avocados_2019 = pd.DataFrame(avocados_dict)
-
-# Print the new DataFrame
-print(avocados_2019)
+#4.Missing Value
+print(avocados_2016.isna())# Check individual values for missing values
+print(avocados_2016.isna().any())# Check each column for missing values
+avocados_2016.isna().sum().plot(kind="bar")# Bar plot of missing values by variable
